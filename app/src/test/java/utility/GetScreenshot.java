53bf6ed2989ad7b9562eabcf81f715babf49785f
AzhanBuzt <infoattechbuzz@gmail.com>
2019-04-06 02:34:03 +0530

    Directory path update for failed screenshot

--- app/src/test/java/utility/GetScreenshot.java
+++ app/src/test/java/utility/GetScreenshot.java
@@ -1,10 +1,15 @@
 package utility;
 
+
+
+import android.app.Application;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import java.io.File;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,9 +28,9 @@ public class GetScreenshot {
     public static String CaptureScreenshotForFailTestCase(WebDriver driver, String name)throws Exception
     {
         TakesScreenshot tk= (TakesScreenshot)driver;
-        String dest="../app/src/screenshot/failed/"+name+".png";
+        String dest= System.getProperty("user.dir")+"/src/screenshot/failed/"+name+".png";
         File source= tk.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source,new File("../app/src/screenshot/failed/"+name+".png"));
+        FileUtils.copyFile(source,new File(dest));
         return dest;
     }
 }
